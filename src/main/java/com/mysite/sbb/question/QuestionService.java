724a8ac68a8a373da08c36f49596ab73acde2adc
hojunking96 <hojunking96@gmail.com>
2023-03-23 11:45:50 +0900

    템플릿 상속 적용 & 질문 등록

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -4,6 +4,7 @@ import com.mysite.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,12 +17,21 @@ public class QuestionService {
         return questionRepository.findAll();
     }
 
-
     public Question getQuestion(Integer id) {
         Optional<Question> oq = questionRepository.findById(id);
 
         if (oq.isPresent() == false) throw new DataNotFoundException("question not found");
 
         return oq.get();
+    }
+
+    public Question create(String subject, String content) {
+        Question q = new Question();
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        questionRepository.save(q);
+
+        return q;
     }
 }
