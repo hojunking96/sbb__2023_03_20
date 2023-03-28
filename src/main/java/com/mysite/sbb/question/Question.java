a2706ea94a48ad12e537509ae0316a40b3099fd1
hojunking96 <hojunking96@gmail.com>
2023-03-28 10:56:53 +0900

    @LazyCollection(LazyCollectionOption.EXTRA) 를 적용하여 size() 시에는 SELECT COUNT, 하지만 다시 N + 1 발생

--- src/main/java/com/mysite/sbb/question/Question.java
+++ src/main/java/com/mysite/sbb/question/Question.java
@@ -4,11 +4,13 @@ import com.mysite.sbb.answer.Answer;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import lombok.ToString;
 
 // question 테이블이 생김
 @Getter
@@ -28,7 +30,6 @@ public class Question {
 
     private LocalDateTime createDate; // DATETIME
 
-
     // OneToMany 자바세상에서의 편의를 위해서 필드 생성
     // 이 녀석은 실제 DB 테이블에 칼럼이 생성되지 않는다.
     // DB는 배열이나 리스트를 저장할 수 없다.
@@ -36,6 +37,7 @@ public class Question {
     // 만들어도 되고 안 만들어도 됩니다.
     // 다만 만들면 해당 객체(질문객체)에서 관련된 답변들을 찾을 때 편합니다.
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    @LazyCollection(LazyCollectionOption.EXTRA) // answerList.size(); 함수가 실행될 때 SELECT COUNT 실행
     // OneToMany 에는 직접객체초기화
     private List<Answer> answerList = new ArrayList<>();
 
